f2279a6f532066e2cb609ea39ef883f990a62ae2
Fakyouka <maksimstar0808@gmail.com>
2026-01-30 22:05:01 +0300

    Bug-Fix

---
+++ Views/Controls/TopBar.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Mobius.Views.Controls
+{
+    public partial class TopBar : UserControl
+    {
+        public TopBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
