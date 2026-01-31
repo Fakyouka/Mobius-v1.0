18eb9d0ac2b94e7b8685dd56004755c6a69095e9
Fakyouka <maksimstar0808@gmail.com>
2026-01-31 13:29:05 +0300

    update

---
+++ Services/Steam/SteamGameResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Mobius.Services.Steam
+{
+    /// <summary>
+    /// Результат поиска по Steam Store.
+    /// </summary>
+    public sealed class SteamGameResult
+    {
+        public int AppId { get; set; }
+        public string Name { get; set; }
+
+        // Удобно для биндинга/отладки
+        public override string ToString() => $"{Name} ({AppId})";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using Mobius.Utils;
+using System.Runtime.CompilerServices;
+
+namespace Mobius.ViewModels
+{
+    /// <summary>
+    /// Базовый VM для проекта: совместим и с Set(...), и с классическим OnPropertyChanged.
+    /// </summary>
+    public abstract class ViewModelBase : ObservableObject
+    {
+        protected void OnPropertyChanged([CallerMemberName] string prop = null)
+            => Raise(prop);
+    }
+}
